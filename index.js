ae80f4c9b0b12712b9d0cc2c510642fc20b0b79e
Angel Davila <adavila@arkusnexus.com>
2023-03-20 22:05:06 -0600

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
@@ -10,7 +11,7 @@ mongoose.connect(process.env.databaseConnectionString, { useNewUrlParser: true }
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use('/api', routes);
 
 // Start server
