6bedbc84b69aac5bb13fd1f1b22a681986215869
AngelDavilaG <angeldavila@drata.com>
2023-03-27 20:40:09 -0600

    rollback

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use('/api', routes);
 
 // Start server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
